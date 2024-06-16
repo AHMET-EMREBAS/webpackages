7e9beac334a639dae7d9206f459ccc4d6e25ccab
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-06-15 19:49:20 -0500

    angular material is added to project

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, MatButtonModule, MatIconModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
