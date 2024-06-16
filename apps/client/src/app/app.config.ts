066c66c88ab897ca9c32238826392f9098b96066
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-06-15 19:55:58 -0500

    app interceptor

--- apps/client/src/app/app.config.ts
+++ apps/client/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
@@ -7,16 +11,21 @@ import { provideStore } from '@ngrx/store';
 import { provideEntityData, withEffects } from '@ngrx/data';
 import { entityConfig } from './entity-metadata';
 import { provideEffects } from '@ngrx/effects';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { AppInterceptor } from './app.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes), provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    }), provideAnimationsAsync(),
+    provideRouter(appRoutes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([AppInterceptor])),
+    provideEffects(),
     provideStore(),
     provideEntityData(entityConfig, withEffects()),
-    provideEffects()
-],
+  ],
 };
