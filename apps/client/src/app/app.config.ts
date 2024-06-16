4878cdcf52cd027690a9375a3297cf2550abebf5
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-06-15 19:53:05 -0500

    ngrx pakcages and configurations

--- apps/client/src/app/app.config.ts
+++ apps/client/src/app/app.config.ts
@@ -3,13 +3,20 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore } from '@ngrx/store';
+import { provideEntityData, withEffects } from '@ngrx/data';
+import { entityConfig } from './entity-metadata';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }), provideAnimationsAsync(),
-  ],
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }), provideAnimationsAsync(),
+    provideStore(),
+    provideEntityData(entityConfig, withEffects()),
+    provideEffects()
+],
 };
