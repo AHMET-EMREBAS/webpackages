3f805e332857e2489315cae47fe643df824fcff3
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-06-20 00:48:04 -0500

    create product

--- libs/clients/src/lib/inventory-management/product/create-product/create-product.component.ts
+++ libs/clients/src/lib/inventory-management/product/create-product/create-product.component.ts
@@ -1,11 +1,54 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'lib-create-product',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatAutocompleteModule,
+  ],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.css',
 })
-export class CreateProductComponent {}
+export class CreateProductComponent {
+  hasSerialNumberControl = new FormControl(false);
+
+  productFormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(100),
+    ]),
+    brand: new FormControl('', [Validators.maxLength(100)]),
+    description: new FormControl('', [Validators.maxLength(1000)]),
+  });
+
+  saveProduct() {
+    console.log(this.productFormGroup.value);
+  }
+}
--- libs/clients/src/lib/inventory-management/product/product.component.ts
+++ libs/clients/src/lib/inventory-management/product/product.component.ts
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'lib-product',
@@ -22,4 +23,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
-export class ProductComponent {}
+export class ProductComponent {
+  handset$ = this.breakpointObserver.observe([
+    Breakpoints.Handset,
+    Breakpoints.HandsetPortrait,
+    Breakpoints.Small,
+  ]);
+
+  constructor(protected readonly breakpointObserver: BreakpointObserver) {}
+}
