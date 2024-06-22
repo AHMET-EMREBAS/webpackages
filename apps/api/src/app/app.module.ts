15c865c31f27fc969163f94ec42f3ed309006971
midtronix <ahmet@midtronix.com>
2024-06-22 09:20:22 -0500

    subscribers

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CommonAppModule } from '@webpackages/boot-nest';
 import * as RestModules from '@webpackages/controllers';
+import * as Subscribers from '@webpackages/entities';
 import { DatabaseModule } from '@webpackages/database';
-import { ProductSubscriber } from '@webpackages/entities';
 
 @Module({
   imports: [
     CommonAppModule,
     DatabaseModule.configure({
-      subscribers: [ProductSubscriber],
+      subscribers: [
+        ...Object.values(Subscribers).filter((e) =>
+          e.name.endsWith('Susbscriber')
+        ),
+      ],
     }),
     ...Object.values(RestModules).filter((e) => e.name.endsWith('Module')),
   ],
