8f070335be0c0451f323c5b9e7018ca150c27a23
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-06-22 23:31:47 -0500

    select input

--- libs/material/input/src/lib/select/select.component.stories.ts
+++ libs/material/input/src/lib/select/select.component.stories.ts
@@ -1,24 +1,40 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import {
+  applicationConfig,
+  type Meta,
+  type StoryObj,
+} from '@storybook/angular';
 import { SelectComponent } from './select.component';
 
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
+import { INPUT_STORY_PROVIDERS } from '../__story';
+import { FormControl } from '@angular/forms';
+import { getBuiltinCategories } from '@webpackages/types';
 
 const meta: Meta<SelectComponent> = {
   component: SelectComponent,
   title: 'SelectComponent',
+  decorators: [
+    applicationConfig({
+      providers: [INPUT_STORY_PROVIDERS],
+    }),
+  ],
 };
 export default meta;
 type Story = StoryObj<SelectComponent>;
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    inputLabel: 'Select Item',
+    inputControl: new FormControl('', []),
+    selectOptions: getBuiltinCategories()
+  },
 };
 
 export const Heading: Story = {
-  args: {},
+  ...Primary,
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    expect(canvas.getByText(/select works!/gi)).toBeTruthy();
+    expect(canvas.getByText(/Select Item/gi)).toBeTruthy();
   },
 };
--- libs/material/input/src/lib/select/select.component.ts
+++ libs/material/input/src/lib/select/select.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { AutocompleteComponent } from '../autocomplete/autocomplete.component';
+import { InputModules } from '../input';
+import { MatSelectModule } from '@angular/material/select';
+import { EntitySelectOption } from '@webpackages/types';
 
 @Component({
   selector: 'wp-select',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>select works!</p>`,
-  styles: ``,
+  imports: [InputModules, MatSelectModule],
+  template: `
+    <mat-select>
+      <mat-label>{{ inputLabel }}</mat-label>
+      @for(option of selectOptions; track option){
+      <mat-option [value]="option">{{ option }}</mat-option>
+      }
+    </mat-select>
+  `,
 })
-export class SelectComponent {}
+export class SelectComponent extends AutocompleteComponent {
+  @Input() selectOptions: EntitySelectOption[];
+}
