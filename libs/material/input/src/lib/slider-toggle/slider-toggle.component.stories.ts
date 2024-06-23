c96722d4c84745bfe84ffe333ac56c1205a00088
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-06-22 23:00:52 -0500

    slide toggle

--- libs/material/input/src/lib/slider-toggle/slider-toggle.component.stories.ts
+++ libs/material/input/src/lib/slider-toggle/slider-toggle.component.stories.ts
@@ -1,18 +1,33 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import {
+  applicationConfig,
+  type Meta,
+  type StoryObj,
+} from '@storybook/angular';
 import { SliderToggleComponent } from './slider-toggle.component';
 
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
+import { INPUT_STORY_PROVIDERS } from '../__story';
+import { FormControl } from '@angular/forms';
 
 const meta: Meta<SliderToggleComponent> = {
   component: SliderToggleComponent,
   title: 'SliderToggleComponent',
+
+  decorators: [
+    applicationConfig({
+      providers: [...INPUT_STORY_PROVIDERS],
+    }),
+  ],
 };
 export default meta;
 type Story = StoryObj<SliderToggleComponent>;
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    inputControl: new FormControl(true, []),
+    inputLabel: 'Is Active',
+  },
 };
 
 export const Heading: Story = {
--- libs/material/input/src/lib/slider-toggle/slider-toggle.component.ts
+++ libs/material/input/src/lib/slider-toggle/slider-toggle.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { InputComponent, InputModules } from '../input';
 @Component({
   selector: 'wp-slider-toggle',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>slider-toggle works!</p>`,
-  styles: ``,
+  imports: [InputModules, MatSlideToggleModule],
+  template: `
+    <div class="flex flex-row gap-4 w-60">
+      <mat-label>{{ inputLabel }}</mat-label>
+      <mat-slide-toggle [formControl]="inputControl"></mat-slide-toggle>
+    </div>
+  `,
 })
-export class SliderToggleComponent {}
+export class SliderToggleComponent extends InputComponent {}
