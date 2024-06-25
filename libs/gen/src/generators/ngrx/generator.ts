12840816c6a7f0cdf35f8a63b83b00afdc9d0192
midtronix <ahmet@midtronix.com>
2024-06-25 12:06:17 -0500

    ngrx services

---
+++ libs/gen/src/generators/ngrx/generator.ts
@@ -0,0 +1,18 @@
+import { formatFiles, generateFiles, names, Tree } from '@nx/devkit';
+import * as path from 'path';
+import * as Metadata from '@webpackages/metadata';
+export async function ngrxGenerator(tree: Tree) {
+  const projectRoot = `libs/clients/ngrx/src/lib`;
+
+  for (const [key] of Object.entries(Metadata)) {
+    const N = names(key.replace('Metadata', ''));
+    generateFiles(tree, path.join(__dirname, 'files'), projectRoot, {
+      ...N,
+    });
+  }
+
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, {});
+  await formatFiles(tree);
+}
+
+export default ngrxGenerator;
