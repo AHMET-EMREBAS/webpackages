dab69ff87c9150e4ca69b60d014742b78e740484
midtronix <ahmet@midtronix.com>
2024-06-27 18:30:23 -0500

    forms generation

---
+++ libs/clients/forms/src/lib/product-form/product-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'wp-product-form',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>product-form works!</p>`,
+  styles: ``,
+})
+export class ProductFormComponent {}
