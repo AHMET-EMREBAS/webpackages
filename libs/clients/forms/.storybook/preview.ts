8cdeb01334e81c113bfe8c220caa27971aafdd52
midtronix <ahmet@midtronix.com>
2024-06-28 12:07:54 -0500

    Forms

--- libs/clients/forms/.storybook/preview.ts
+++ libs/clients/forms/.storybook/preview.ts
@@ -1,8 +1,39 @@
 // Replace your-framework with the framework you are using (e.g., react, vue3)
-import { Preview } from '@storybook/angular';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideEntityData, withEffects } from '@ngrx/data';
+import { Preview, applicationConfig } from '@storybook/angular';
+import { entityConfig } from '@webpackages/clients/ngrx';
+import {
+  provideDefaultInputDebounceTime,
+  provideDefaultInputErrorMesssageHandler,
+  provideDefaultInputStatusIndicatorHandler,
+} from '@webpackages/material/input';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import {
+  provideDefaultHttpCountQueryBuilder,
+  provideDefaultHttpSearchQueryBuilder,
+} from '@webpackages/material/core';
 
 const preview: Preview = {
   parameters: {},
+  decorators: [
+    applicationConfig({
+      providers: [
+        provideHttpClient(),
+        provideAnimations(),
+        provideStore(),
+        provideEffects(),
+        provideEntityData(entityConfig, withEffects()),
+        provideDefaultInputDebounceTime(),
+        provideDefaultInputErrorMesssageHandler(),
+        provideDefaultInputStatusIndicatorHandler(),
+        provideDefaultHttpCountQueryBuilder(),
+        provideDefaultHttpSearchQueryBuilder(),
+      ],
+    }),
+  ],
 };
 
 export default preview;
