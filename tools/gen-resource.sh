3baa8176499aa7846d18a92304d0dda9495e8343
midtronix <ahmet@midtronix.com>
2024-06-28 11:20:32 -0500

    resource generator script

---
+++ tools/gen-resource.sh
@@ -0,0 +1,16 @@
+ npx nx g @webpackages/gen:model $1
+ npx nx g @webpackages/gen:entity $1
+ npx nx g @webpackages/gen:entity-list $1
+ 
+ npx nx g @webpackages/gen:resource $1
+ npx nx g @webpackages/gen:resource-list $1
+
+ npx nx g @webpackages/gen:ngrx $1
+ npx nx g @webpackages/gen:ngrx-config $1
+
+ npx nx g @webpackages/gen:form $1
+ npx nx g @webpackages/gen:form-group $1
+
+ npx nx g @webpackages/gen:routes $1
+
+
