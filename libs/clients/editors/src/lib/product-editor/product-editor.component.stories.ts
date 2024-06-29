1e58cac1dd9dc9de3a32390e27605ac329945b1a
midtronix <ahmet@midtronix.com>
2024-06-29 12:27:47 -0500

    material-form storybook

--- libs/clients/editors/src/lib/product-editor/product-editor.component.stories.ts
+++ libs/clients/editors/src/lib/product-editor/product-editor.component.stories.ts
@@ -1,14 +1,15 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { type Meta, type StoryObj } from '@storybook/angular';
 import { ProductEditorComponent } from './product-editor.component';
 
 import { within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
 
 const meta: Meta<ProductEditorComponent> = {
   component: ProductEditorComponent,
   title: 'ProductEditorComponent',
 };
+
 export default meta;
+
 type Story = StoryObj<ProductEditorComponent>;
 
 export const Primary: Story = {};
