96cd9b99478a11af6cfecefc64de96c434ef1016
midtronix <ahmet@midtronix.com>
2024-07-01 09:42:16 -0500

    quantity editor

---
+++ libs/clients/editors/src/lib/quantity-editor/quantity-editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'wp-quantity-editor',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>quantity-editor works!</p>`,
+  styles: ``,
+})
+export class QuantityEditorComponent {}
